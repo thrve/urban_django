ed1ac2a390e44aa11445c993cc5473bacd193bd8
thrve <hithrv@yandex.ru>
2025-01-03 03:39:16 +0300

    change server_ip

--- urban_django/UrbanDjango/UrbanDjango/settings.py
+++ urban_django/UrbanDjango/UrbanDjango/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-6d%)yum@(nd-e&8g&)oxj=*ygnm81!ua$2&nf95l!3r*si0ty&
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    'server_ip',
+    server_ip,
     'localhost',
     '127.0.0.1',
 ]
